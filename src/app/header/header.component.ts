6c2a6190667d40eb38c052507e473f4487f7a829
Angela Zhao <angelazhao@Angelas-MacBook-Pro-2.local>
2019-07-18 13:16:13 -0400

    folder update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchService } from '../search.service';
-import { IReturnData } from '../returnData';
-import { IAlbum } from '../album';
+import { SearchService } from '../services/search.service';
+import { IReturnData } from '../interface/returnData';
+import { IAlbum } from '../interface/album';
 
 @Component({
   selector: 'app-header',
@@ -10,25 +10,31 @@ import { IAlbum } from '../album';
 })
 export class HeaderComponent implements OnInit {
 
-  albumData : IReturnData;
+  albumData: IReturnData;
   resultData: IAlbum[];
-   //name: string;
+   // name: string;
   constructor(private searchService: SearchService) {
 
    }
-  showAlbumResult(value){
+  showAlbumResult(value) {
     this.resultData = [];
     // this.name = value;
-    this.searchService.getAlbums(value).subscribe(data=>{this.albumData=data; this.resultData = this.albumData.results; console.log(this.albumData.results);});
+    // tslint:disable-next-line: max-line-length
+    this.searchService.getAlbums(value)
+    .subscribe(
+      data => {
+        this.albumData = data;
+        this.resultData = this.albumData.results;
+        console.log(this.albumData.results); });
   }
 
- 
 
-  ngOnInit() {  
-    //this.searchService.getAlbums(name).subscribe(data=>this.albumData=data);
+
+  ngOnInit() {
+    // this.searchService.getAlbums(name).subscribe(data=>this.albumData=data);
     //  console.log(this.albumData);
   }
 
-  
+
 
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IReturnData } from './returnData';
+import { IReturnData } from '../interface/returnData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  itunes_url: string = "https://itunes.apple.com/search?term="
-  album_url:string="&entiry=album"
+  itunes_url = 'https://itunes.apple.com/search?term=';
+  album_url = '&entiry=album';
 
   constructor(private http: HttpClient) {
 
    }
-   getAlbums(artistName){
+   getAlbums(artistName): Observable<IReturnData> {
     // return this.http.get<IReturnData>(`${this.itunes_url}${artistName}${this.album_url}`);
-    return this.http.get<IReturnData>(this.itunes_url+artistName+this.album_url);
-    
+    return this.http.get<IReturnData>(this.itunes_url + artistName + this.album_url);
+
    }
 }
